506ed988e542082c27ba765828a60742cff46e17
Chris <kjman26@gmail.com>
2022-08-26 08:58:52 -0700

    functioning password generator, need to add readme

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -2,13 +2,14 @@
 
 // arrays for possible characters
 var generateBtn = document.querySelector("#generate");
-var lower = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-var upper = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-var number = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-var special = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '+', '-', '_', '='];
+
 var possibleChars = "";
 var passLength;
-var password = "";
+var password="";
+const lower = "abcdefghijklmnopqrstuvwxyz";
+const upper ="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const number = "0123456789";
+const special = " #!/$%&'()*+,-.:;<=>?@][^_`{|}~";
 
 
 
@@ -84,14 +85,15 @@ console.log(passLength)
 
   //build the password with possibleChars randomly
 console.log(possibleChars)
-
+if(possibleChars){
   for (i = 0; i <= passLength; i++) {
-    const finalChars = possibleChars[Math.floor(Math.random()*passLength)];
+    const finalChars = possibleChars[Math.floor(Math.random()*possibleChars.length)];
     password = password + finalChars; 
   };
- 
+ }
 console.log(password)
   return password;
+
 };
 
 var generateBtn = document.querySelector("#generate")
