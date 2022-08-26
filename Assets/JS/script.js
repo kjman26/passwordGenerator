92c8462696baa15914b26d681eeb41e3dcf91377
Chris <kjman26@gmail.com>
2022-08-25 18:11:32 -0700

    updated user prefs and added action if they choose nothing

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -2,10 +2,11 @@
 
 // arrays for possible characters
 var generateBtn = document.querySelector("#generate");
-var lower = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-var upper = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
-var number = ['0','1','2','3','4','5','6','7','8','9']
-var special = ['!','@','#','$','%','^','&','*','(',')','+','-']
+var lower = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var upper = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+var number = ['0','1','2','3','4','5','6','7','8','9'];
+var special = ['!','@','#','$','%','^','&','*','(',')','+','-'];
+var possibleChars = "";
 
 //instructions
 
@@ -13,80 +14,78 @@ window.confirm("Password must be 8-128 characters and include a lowercase and up
 
 // ask for number of characters and store answer in userChars
 
-var userChars = window.prompt("Enter number of characters between 1 - 128");
+var passLength = window.prompt("Enter number of characters between 1 - 128");
 
-if(userChars<=128 && userChars>=8){
+if(passLength<=128 && passLength>=8){
   alert("That's my favorite number!")
 }
 else{
   alert("please TRY")
 };
-console.log(userChars);
 
+
+
+console.log(possibleChars+=passLength);
+
+
+;
+
+// Checking user preferences and adding to possibleChars array if user selects ok
 
 // lower case letter 
 var userLower = confirm("Do you want lowercase letters(OK for yes and CANCEL for no)")
   if(userLower == true){
+    possibleChars+=lower;
     alert("Smart!")
   }
   else{
     alert("Will not include LOWERCASE letters!")
   };
-  console.log(userLower);
+  console.log(possibleChars);
  
   //upper case option for user
 var userUpper = confirm("Do you want uppercase letters?")
   if(userUpper == true){
+    possibleChars+=upper;
     alert("Understood!")
+
   }
   else{
     alert("Will not include UPPERCASE letters")
   };
-console.log(userUpper);
+console.log(possibleChars);
 
 //numbers option for user
 var userNum = confirm("Do you want numbers?")
   if(userNum == true){
+    possibleChars+=number;
     alert("Even Safer Still!")
   }
   else{
     alert("Will not include NUMBERS")
   };
-console.log(userNum);
+console.log(possibleChars);
 
 //special character user option
 
 var userSpcl = confirm("Do you want special characters?")
   if(userSpcl == true){
+    possibleChars+=special;
     alert("The Safest")
   }
   else{
     alert("Will not include SPECIAL CHARACTERS")
   };
-console.log(userSpcl)
-//call random char from each
+console.log(possibleChars)
 
-const lowerCase = lower[Math.floor(Math.random() * lower.length)];
-for(i=0; i<lower.length; i++);
-// console.log(lowerCase);
+// if no identifiers are selected
+if(upper === false && lower === false && numbers === false && special === false){
+  console.log("Please select at least ONE unique characteristic!")
+}
 
-const upperCase = upper[Math.floor(Math.random() * upper.length)];
-// console.log(upperCase);
-
-const nums = number[Math.floor(Math.random() * number.length)];
-// console.log(nums);
-
-const specialChars = special[Math.floor(Math.random() * special.length)];
+const finalChars = possibleChars(Math.floor(math.random()*possibleChars.length))
 // console.log(specialChars);
 
-//concat all chosen characters
-
-const ALL = lowerCase.concat(upperCase, nums, specialChars)
-for(i=0; i<ALL; i++);
-
-console.log(ALL);
-
-//ifs! contains a special, else; contains a number, else, contains an upper, else; contains lower,else
 
 //TODO: check if .INCLUDES
 function generatePassword(){
